b5004f7a3ac37e4087a1aa4c298a66ca3b4174ea
kecherou <j.kesraoui@pimenko.com>
2025-01-08 14:26:16 +0700

    [PIMDEV-581] Fix a bug where a item is not owned when user complete an activity in avatar game element.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_ludimoodle';
-$plugin->release = '0.1.0';
-$plugin->version = 2024120600;
+$plugin->release = '1.0.0';
+$plugin->version = 2025010700;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
